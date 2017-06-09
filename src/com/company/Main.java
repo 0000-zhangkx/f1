6df765699792405504a314a224e3000fc47c090e
0000-zhangkx <592557982@qq.com>
2017-06-09 20:31:57 +0800

    本次使用commit小图标上传

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("dear+git");
+        System.out.println("本次是使用commit小图标上床");
     }
 }
